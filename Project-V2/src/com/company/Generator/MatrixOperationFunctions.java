c9435e2e35fd33503e257330cbdeb15707477c5f
Morten Timmermann <mtimme16@student.aau.dk>
2018-05-20 20:23:43 +0200

    Update on built in functions

--- Project-V2/src/com/company/Generator/MatrixOperationFunctions.java
+++ Project-V2/src/com/company/Generator/MatrixOperationFunctions.java
@@ -10,10 +10,15 @@ public class MatrixOperationFunctions
     {
 
         String code = "";
+        code += "__device__ void "
         code += "MatrixAdd(";
         code += target.Device("M_ONE") + ", ";
         code += target.Device("M_TWO") + ", ";
-        code += target.Device("TARGET") + ")";
+        code += target.Device("TARGET") + "){";
+        code += emit("int i = threadIdx.x;");
+        code += emit("int j = threadIdx.y;");
+        code += emit(target.Device("TARGET") + "[i][j] =" + target.Device("M_ONE") + "[i][j] + " + target.Device("M_TWO") + "[i][j];")
+        code += emit("}")
         return code;
     }
 
@@ -21,10 +26,15 @@ public class MatrixOperationFunctions
     {
 
         String code = "";
+        code += "__device__ void "
         code += "MatrixSub(";
         code += target.Device("M_ONE") + ", ";
         code += target.Device("M_TWO") + ", ";
-        code += target.Device("TARGET") + ")";
+        code += target.Device("TARGET") + "){";
+        code += emit("int i = threadIdx.x;");
+        code += emit("int j = threadIdx.y;");
+        code += emit(target.Device("TARGET") + "[i][j] =" + target.Device("M_ONE") + "[i][j] - " + target.Device("M_TWO") + "[i][j];")
+        code += emit("}")
         return code;
     }
 
@@ -36,6 +46,22 @@ public class MatrixOperationFunctions
         code += target.Device("M_ONE") + ", ";
         code += target.Device("M_TWO") + ", ";
         code += target.Device("TARGET") + ")";
+        return code;
+    }
+
+    public static String MatrixTrans (Target target)
+    {
+        String code = "";
+        code += "__device__ void "
+        code += "MatrixTrans(";
+        code += target.Device("M_ONE") + ", ";
+        code += target.Device("TARGET") + "){";
+        code += emit("int x = blockIdx.x * blockDim.x + threadIdx.x;");
+        code += emit("int y = blockIdx.y * blockDim.y + threadIdx.y;");
+        code += emit("if(x >= matrixSize || y>= matrixSize)");
+        code += emit("return;");
+        code += emit(target.Device("TARGET") + "[x][y] = " + target.Device("M_ONE") + "[x][y];");
+        code += emit("}")
         return code;
     }
 }
